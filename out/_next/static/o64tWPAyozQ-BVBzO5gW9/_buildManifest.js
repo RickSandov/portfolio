fda5b6c179cf2ca9d06b0a42e0b90e01e1fca5a0
Ricardo <i.s.ricardo.sandoval@gmail.com>
2022-05-09 15:34:20 -0500

    static site

---
+++ out/_next/static/o64tWPAyozQ-BVBzO5gW9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/664-d0f151b9ff4fbe60.js","static/css/248e6f59ead0dc40.css","static/chunks/pages/index-c47a1b734ff72826.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
